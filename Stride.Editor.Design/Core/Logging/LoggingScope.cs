a94e888ce2018154871126c2f11be298732b7849
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-10-13 14:48:15 +0100

    Add TimedScope logging

---
+++ Stride.Editor.Design/Core/Logging/LoggingScope.cs
@@ -0,0 +1,51 @@
+﻿using Stride.Core.Diagnostics;
+
+namespace Stride.Editor.Design.Core.Logging
+{
+    /// <summary>
+    /// A named scope unit connected of the <see cref="TimedScope"/>. Use <see cref="LoggingScope"/> to include scope stack information.
+    /// </summary>
+    public class LoggingScope : ILogger
+    {
+        public LoggingScope(ILogger logger)
+            => this.Logger = logger;
+
+        /// <inheritdoc/>
+        public string Module => Logger.Module;
+
+        /// <summary>
+        /// Target to log into.
+        /// </summary>
+        private ILogger Logger { get; }
+
+        /// <inheritdoc/>
+        /// <remarks>Includes <see cref="TimedScope.CurrentScopePath"/> information.</remarks>
+        public void Log(ILogMessage logMessage)
+        {
+            var path = TimedScope.CurrentScopePath;
+            (logMessage as LogMessage).Module = TimedScope.IsCurrentLoggingScope(this) 
+                ? path
+                : $"{path}{(string.IsNullOrEmpty(path) ? "" : "/")}{Module}";
+
+            Logger.Log(logMessage);
+        }
+
+        /// <summary>
+        /// (Only when initialized with a <see cref="Stride.Core.Diagnostics.Logger"/> instance) <br/>
+        /// Activates the log for this logger for a range of <see cref="LogMessageType"/>.
+        /// </summary>
+        /// <param name="fromLevel">The lowest inclusive level to log for.</param>
+        /// <param name="toLevel">The highest inclusive level to log for.</param>
+        /// <param name="enabledFlag">if set to <c>true</c> this will enable the log, false otherwise. Default is true.</param>
+        /// <remarks>
+        /// Outside the specified range the log message type are disabled (!enabledFlag).
+        /// </remarks>
+        public void ActivateLog(LogMessageType fromLevel, LogMessageType toLevel = LogMessageType.Fatal, bool enabledFlag = true)
+        {
+            if (Logger is Logger log)
+                log.ActivateLog(fromLevel, toLevel, enabledFlag);
+            else 
+                throw new System.NotSupportedException($"The target logger does not inherit from '{typeof(Logger).FullName}'.");
+        }
+    }
+}
---
+++ Stride.Editor.Design/Core/Logging/TimedScope.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Stride.Core.Diagnostics;
+
+namespace Stride.Editor.Design.Core.Logging
+{
+    /// <summary>
+    /// Logging scope which also measures execution time. Safe to use across async calls
+    /// </summary>
+    /// <example>
+    /// static LoggingScope MyScope = new LoggingScope(GlobalLogger.GetLogger(nameof(MyClass)));
+    /// 
+    /// using(var scope = new TimedScope(MyScope, startWith: TimedScope.Status.Failure))
+    /// {
+    ///     try
+    ///     {
+    ///         MyWork();
+    ///         scope.Result = TimedScope.Status.Success;
+    ///     } catch (Exception e)
+    ///     {
+    ///         scope.Error(e.Message, e);
+    ///         throw;
+    ///     }
+    /// }
+    /// </example>
+    public class TimedScope : IDisposable, ILogger
+    {
+        public enum Status
+        {
+            Failure = 0,
+            Success = 1,
+            Warning = 2,
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="TimedScope"/>, logs start info and starts the stopwatch.
+        /// </summary>
+        /// <param name="loggingScope">Logging scope to be used - the reported scope name comes form <see cref="LoggingScope.Module"/> property.</param>
+        /// <param name="startWith">Initial result status - use <see cref="Status.Failure"/> when the action may throw.</param>
+        /// <remarks>Use a <c>using</c> block when possible, otherwise you have to remember to call <see cref="Dispose"/>.</remarks>
+        public TimedScope(LoggingScope loggingScope, Status startWith = Status.Success)
+        {
+            LoggingScope = loggingScope;
+            Result = startWith;
+            ScopeStack = ScopeStack.Push(this);
+
+            LoggingScope.Info("TimedScope started.");
+            Stopwatch = Stopwatch.StartNew();
+        }
+
+        private LoggingScope LoggingScope { get; }
+
+        private Stopwatch Stopwatch { get; }
+
+        /// <summary>
+        /// Result reported when the <see cref="TimedScope"/> finishes.
+        /// </summary>
+        public Status Result { get; set; }
+
+        private void Stop()
+        {
+            Stopwatch.Stop();
+
+            var message = $"TimedScope finished with {Result} in {GetElapsedTime()}.";
+            switch (Result)
+            {
+                case Status.Failure:
+                    LoggingScope.Error(message);
+                    break;
+                case Status.Success:
+                    LoggingScope.Info(message);
+                    break;
+                case Status.Warning:
+                    LoggingScope.Warning(message);
+                    break;
+                default:
+                    throw new NotSupportedException($"Status value '{Result}' is not supported.");
+            }
+        }
+
+        private string GetElapsedTime()
+        {
+            var elapsed = Stopwatch.Elapsed;
+            if (elapsed.TotalMinutes > 1)
+                return $"{elapsed.TotalMinutes} min";
+            else if (elapsed.TotalSeconds > 1)
+                return $"{elapsed.TotalSeconds} sec";
+            else return $"{elapsed.TotalMilliseconds} ms";
+        }
+
+        /// <summary>
+        /// Stops the stopwatch, logs the result, pops nested scope stack.
+        /// </summary>
+        public void Dispose()
+        {
+            Stop();
+            ScopeStack = ScopeStack.Pop();
+        }
+
+        string ILogger.Module => CurrentScopePath; // assumes scope is always stack local
+        void ILogger.Log(ILogMessage logMessage) => LoggingScope.Log(logMessage);
+
+        public static string CurrentScopePath
+        {
+            get
+            {
+                var stack = ScopeStack.Select(ts => ts.LoggingScope.Module).ToArray();
+                if (stack.Length == 0)
+                    return "";
+                return $"{string.Join("/", stack.Reverse())}";
+            }
+        }
+
+        internal static bool IsCurrentLoggingScope(LoggingScope scope)
+            => !ScopeStack.IsEmpty && ScopeStack.Peek().LoggingScope == scope;
+
+        private static readonly AsyncLocal<ImmutableStack<TimedScope>> LocalScopeStack = new AsyncLocal<ImmutableStack<TimedScope>>();
+
+        private static ImmutableStack<TimedScope> ScopeStack
+        {
+            get
+            {
+                if (LocalScopeStack.Value == null)
+                    LocalScopeStack.Value = ImmutableStack<TimedScope>.Empty;
+                return LocalScopeStack.Value;
+            }
+            set
+            {
+                LocalScopeStack.Value = value;
+            }
+        }
+    }
+}
